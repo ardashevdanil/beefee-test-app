7d0e300758bedfd1094e27ad2a202dcc13cde0f0
Danil Ardashev <ardashev_danil@yahoo.com>
2019-11-02 16:19:58 +0300

    Updates

---
+++ precache-manifest.0d5f8fe7357264da1141eb83303cf939.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a788e825c4a698aab8231767e22eaae",
+    "url": "/beejee-test-app/index.html"
+  },
+  {
+    "revision": "f8232ff2ab17e5d72751",
+    "url": "/beejee-test-app/static/js/2.748ffd00.chunk.js"
+  },
+  {
+    "revision": "48e595d731c83ab8429a",
+    "url": "/beejee-test-app/static/js/main.dd783845.chunk.js"
+  },
+  {
+    "revision": "b0053d601b95b5a59bcd",
+    "url": "/beejee-test-app/static/js/runtime-main.2ce1ee25.js"
+  }
+]);
